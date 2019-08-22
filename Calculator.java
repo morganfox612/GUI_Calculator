0f3263f74fbea3f193d4046264ecb999f6e15510
Jason Calixto <JasonCalixto97@gmail.com>
2019-08-22 10:19:39 -0400

    making buttons

--- Calculator.java
+++ Calculator.java
@@ -24,5 +24,30 @@ public class Calculator
         createAndShowGUI();
       }
     });
+    JButton zero = new JButton("0");
+    JButton one = new JButton("1");
+    JButton two = new JButton("2");
+    JButton three = new JButton("3");
+    JButton four = new JButton("4");
+    JButton five = new JButton("5");
+    JButton six = new JButton("6");
+    JButton seven = new JButton("7");
+    JButton eight = new JButton("8");
+    JButton nine = new JButton("9");
+    JButton plus = new JButton("+");
+    JButton minus = new JButton("-");
+    zero.addActionListener(calculator);
+    one.addActionListener(calculator);
+    two.addActionListener(calculator);
+    three.addActionListener(calculator);
+    four.addActionListener(calculator);
+    five.addActionListener(calculator);
+    six.addActionListener(calculator);
+    seven.addActionListener(calculator);
+    eight.addActionListener(calculator);
+    nine.addActionListener(calculator);
+    plus.addActionListener(calculator);
+    minus.addActionListener(calculator);
+
   }
 }
